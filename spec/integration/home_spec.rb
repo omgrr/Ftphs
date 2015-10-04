25bf9141eb969c6bfdabbd790e8b68b4de5c3524
omgrr <jerwis0731@gmail.com>
2015-10-04 14:36:11 -0500

    Add user naem and rank to home page

--- spec/integration/home_spec.rb
+++ spec/integration/home_spec.rb
@@ -20,4 +20,16 @@ describe "the home page", :type => :feature do
     expect(page).to have_selector(".user##{omgrr.id}")
     expect(page).to have_selector(".user##{bison.id}")
   end
+
+  it "displays the users name and rank" do
+    omgrr = User.create(name: "omgrr", rank: 20)
+    bison = User.create(name: "bison", rank: 25)
+
+    visit "/"
+
+    expect(find(".user##{omgrr.id}")).to have_content("omgrr")
+    expect(find(".user##{omgrr.id}")).to have_content("20")
+    expect(find(".user##{bison.id}")).to have_content("bison")
+    expect(find(".user##{bison.id}")).to have_content("25")
+  end
 end
